0f37ad7b626d6e12451e7b34ffc0be437a102ea1
AmeerHussainEmumba <ameer.hussain@emumba.com>
2022-02-10 23:37:04 +0500

    no message

--- src/main/java/Pages/lotionPage.java
+++ src/main/java/Pages/lotionPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 
 import java.lang.annotation.ElementType;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,22 +26,25 @@ public class lotionPage {
         ArrayList<Integer> selectedElements = new ArrayList<>();
         List<WebElement> elementPrices = new ArrayList<>();
         List<Integer> selectedElementPrices = new ArrayList<>();
+        List<Integer> sortedList=new ArrayList<>();
+        int selection1 = 99;
         if (decisionTemp < 19) {
             Elements = driver.findElements(By.xpath("//html/body//p[1][not(contains(.,'Services'))]"));
-            for (int counter = 0; counter <= Elements.size() - 1; counter++) {
-                if (Elements.get(counter).getText().contains("Aloe") || Elements.get(counter).getText().contains("aloe")) {
-                    selectedElements.add(counter);
+            for (int position = 0; position <= Elements.size() - 1; position++) {
+                if (Elements.get(position).getText().contains("Aloe") || Elements.get(position).getText().contains("aloe")) {
+                    selectedElements.add(position);
                 } else {}
             }
             elementPrices = driver.findElements(By.xpath("//html/body//p[2]"));
             for (int counter = 0; counter <= selectedElements.size() - 1; counter++) {
-
                 Pattern intsOnly = Pattern.compile("\\d+");
                 Matcher makeMatch = intsOnly.matcher(elementPrices.get(selectedElements.get(counter)).getText());
                 makeMatch.find();
                 selectedElementPrices.add(Integer.parseInt(makeMatch.group()));
-
             }
+           int temp=selectedElementPrices.indexOf((Collections.min(selectedElementPrices)));
+            selection1= selectedElements.get(temp);
+
         } else { //sunscreen
             Elements = driver.findElements(By.xpath("//html/body//p[1][not(contains(.,'Services'))]"));
 
@@ -55,7 +60,10 @@ public class lotionPage {
                 makeMatch.find();
                 selectedElementPrices.add(Integer.parseInt(makeMatch.group()));
             }
+            int temp=selectedElementPrices.indexOf((Collections.min(selectedElementPrices)));
+            selection1= selectedElements.get(temp);
         }
+        //Collections.min(selectedElementPrices);
             System.out.println("number of selected element " + selectedElements.size());
             System.out.println("number of selected elements according to prices " + selectedElementPrices.size());
             for (int a = 0; a <= selectedElements.size() - 1; a++) {
@@ -64,6 +72,11 @@ public class lotionPage {
             for (int a = 0; a <= selectedElementPrices.size() - 1; a++) {
                 System.out.println(selectedElementPrices.get(a));
             }
+            for (int a = 0; a <= sortedList.size() - 1; a++) {
+            System.out.println(sortedList.get(a));
+        }
+            System.out.println(Collections.min(selectedElementPrices));
+            System.out.println(selection1);
             System.out.println("done");
         }
     }
